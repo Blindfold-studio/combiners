2ef97b92fb690e8c689c950421ac28b56e8efd37
RocKIn0X <dantai.l@ku.th>
2018-07-04 16:15:13 +0300

    Player can get damage from enemy

---
+++ Assets/Scripts/Player/PlayerHitBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerHitBox : MonoBehaviour {
+
+    private HealthSystem playerHP;
+
+    void Start()
+    {
+        playerHP = GameObject.FindGameObjectWithTag("GameController").GetComponent<HealthSystem>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Debug.Log("Player is hit by enemy");
+            playerHP.HP = -1;
+        }    
+
+        else
+        {
+            return;
+        }
+    }
+}
