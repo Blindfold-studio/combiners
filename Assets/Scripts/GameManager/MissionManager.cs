d4ceb2ef6e6fd7e6faf1bb0761a1d3624297bfd2
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2018-07-16 02:14:25 +0300

    add boss health ui in Canvas_P1/P2

---
+++ Assets/Scripts/GameManager/MissionManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour {
+    
+    #region Singleton
+    public static MissionManager instance = null;
+
+    void Awake ()
+    {
+        instance = this;
+    }
+    #endregion
+
+    void Start () {
+		
+	}
+
+}
