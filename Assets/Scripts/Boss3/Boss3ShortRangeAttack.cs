755107dda2e01a2b58c5f4cb72e4aa3fbc8c3a4e
piromsurang <piromsurang.r@ku.th>
2018-07-16 14:28:19 +0300

    make private value serializable in short range attack script

--- Assets/Scripts/Boss3/Boss3ShortRangeAttack.cs
+++ Assets/Scripts/Boss3/Boss3ShortRangeAttack.cs
@@ -8,6 +8,10 @@ public class Boss3ShortRangeAttack : MonoBehaviour {
 	private bool isPlayerInRange;
     private Boss3Movement boss3Movement;
     private GameObject shortRangeWeapon;
+    [SerializeField]
+    private float animWaitTime = 1f;
+    [SerializeField]
+    private float waitAfterAttackTime = 0.5f;
 
     private void Start() {
         isPlayerInRange = false;
@@ -40,9 +44,9 @@ public class Boss3ShortRangeAttack : MonoBehaviour {
         Boss3Movement.StopCoroutineEvent += StopAttack;
         boss3Movement.CurrentState = Boss3Movement.State.IsShortRangeAttacking;
         Debug.Log(boss3Movement.TargetPlayer.name + " was attacked by sword!");
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(animWaitTime);
         shortRangeWeapon.SetActive(true);
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(waitAfterAttackTime);
         shortRangeWeapon.SetActive(false);
         boss3Movement.CurrentState = Boss3Movement.State.Moving;
         Debug.Log("The sword attack stops!");
