98d87205d7ba58957108f8b45e0491204dd6e317
RocKIn0X <dantai.l@ku.th>
2018-07-16 10:36:08 +0300

    show upgrade panel when boss died

--- Assets/Scripts/GameManager/MissionManager.cs
+++ Assets/Scripts/GameManager/MissionManager.cs
@@ -13,14 +13,30 @@ public class MissionManager : MonoBehaviour {
     }
     #endregion
 
+    public GameObject BossObject;
+
     [SerializeField]
     private Transform bossPosition_P1;
     [SerializeField]
     private Transform bossPosition_P2;
 
+    private BossHealth bossHealth;
+    private GameObject upgradePanel;
+
     void Start () {
-		
+        bossHealth = BossObject.GetComponent<BossHealth>();
+        upgradePanel = GameObject.FindGameObjectWithTag("WinAndUpgrade");
+        upgradePanel.SetActive(false);
 	}
+
+    void Update()
+    {
+        if (bossHealth.Health <= 0)
+        {
+            upgradePanel.SetActive(true);
+            Time.timeScale = 0f;
+        }    
+    }
 
     public Transform GetBossPosition_P1 ()
     {
