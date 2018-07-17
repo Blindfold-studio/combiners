1d407c1c95811338d01635d4f5db8da4b419d9e1
RocKIn0X <dantai.l@ku.th>
2018-07-17 11:06:36 +0300

    can click start and go to game scene

---
+++ Assets/Scripts/MainMenu/ScreenManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour {
+
+    private GameManager gameManager;
+	
+	void Start () {
+        gameManager = GameManager.instance;
+	}
+	
+	public void StartGame ()
+    {
+        gameManager.LoadNextScene();
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("Exit game");
+        Application.Quit();
+    }
+}
