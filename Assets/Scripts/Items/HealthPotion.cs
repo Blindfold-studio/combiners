7388c51a96994ba24a0ec2b9dfdcc7dc81cc8e06
RocKIn0X <dantai.l@ku.th>
2018-07-19 17:26:30 +0300

    adjust fly enemy to more close player and fix enemy stick items

--- Assets/Scripts/Items/HealthPotion.cs
+++ Assets/Scripts/Items/HealthPotion.cs
@@ -7,6 +7,7 @@ public class HealthPotion : MonoBehaviour {
 
 	private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("Player")) {
+            Debug.Log("current health: " + HealthSystem.instance.HP);
             HealthSystem.instance.HP = 1;
             this.gameObject.SetActive(false);
         }
--- Assets/Scripts/Items/ItemFloorScript.cs
+++ Assets/Scripts/Items/ItemFloorScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ItemFloorScript : MonoBehaviour {
 
-	private void OnTriggerEnter2D(Collider2D other) {
+	private void OnColliderEnter2D(Collider2D other) {
         if(other.CompareTag("Enemy") || other.CompareTag("Boss")
         || other.CompareTag("Player")) {
             Physics2D.IgnoreCollision(other, this.GetComponent<BoxCollider2D>(), true);
