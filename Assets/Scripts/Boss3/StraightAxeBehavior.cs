7e7bb748d4a94ee476357546d2f6d33bcfc5f4c4
piromsurang <piromsurang.r@ku.th>
2018-07-19 10:46:59 +0300

    add animation in straight axe

--- Assets/Scripts/Boss3/StraightAxeBehavior.cs
+++ Assets/Scripts/Boss3/StraightAxeBehavior.cs
@@ -7,21 +7,24 @@ public class StraightAxeBehavior : MonoBehaviour {
     private Rigidbody2D rb;
     [SerializeField]
     private float axeSpeed = 10f;
+    private Animation anim;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animation>();
         this.gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
+            anim.Stop();
             this.gameObject.SetActive(false);
         }
     }
 
 	public void Moving(GameObject targetPlayer) {
+        anim.Play();
         if(targetPlayer.transform.position.x - transform.position.x < 0) {
-            Debug.Log(rb);
             rb.velocity = new Vector2(-1*axeSpeed, rb.velocity.y);
         } else {
             rb.velocity = new Vector2(axeSpeed, rb.velocity.y);
