96584af5d8397c97122636783a78a8f667701e4d
RocKIn0X <dantai.l@ku.th>
2018-08-02 10:05:30 +0300

    add shield collider to knight boss

---
+++ Assets/Scripts/Boss3/Boss3Shield.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Boss3Shield : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.LogWarning(collision.gameObject.tag);
+
+        if (collision.CompareTag("Weapon"))
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+}
