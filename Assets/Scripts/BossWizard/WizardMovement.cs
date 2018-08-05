25542aa3f035167725b710c17227d5aaf3a2dd7d
Thanawat <Spirit_windwalk@hotmail.com>
2018-08-05 23:17:09 +0700

    Set Boss

--- Assets/Scripts/BossWizard/WizardMovement.cs
+++ Assets/Scripts/BossWizard/WizardMovement.cs
@@ -3,32 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class BossFlyingMovement : Boss {
+public class WizardMovement : Boss {
 
-    [SerializeField]
-    private float rangeX = 10.0f;
-    [SerializeField]
-    private float rangeY = 1.0f;
-    [SerializeField]
-    private float speedX = 1.25f;
-    [SerializeField]
-    private float speedY = 2.5f;
-    public float initiatePoint;
-    private float x, y;
-    private int count = 0;
-    private float offSet;
-    public Vector3 curPosition;
-    public bool inPlayer1;
 
-    public enum State { Idle, Moving, MoveCircle, MoveToCheckBox, HorizontalMove};
+    public enum State { Idle, Moving};
     private State state;
     private MissionManager missionManager;
     private Transform player1_screen;
     private Transform player2_screen;
-    public static event Action StopCoroutineEvent;
-
-
     private GameObject targetPlayer;
+    public static event Action StopCoroutineEvent;
 
     public State CurrentState
     {
@@ -65,42 +49,14 @@ public class BossFlyingMovement : Boss {
         BossHealth.DeathEvent += Die;
     }
 
-    void Start ()
-    {
+    void Start () {
         missionManager = MissionManager.instance;
-        offSet = rangeY * Mathf.Sin(speedY * initiatePoint) + this.transform.position.y;
-        curPosition = this.transform.position;
-        CheckBossPosition();
-        state = State.Moving;
-    }
 
-    void Update()
-    {
-       
+    }
+	
+	
+	void Update () {
         TargetPlayer = FindTheClosestPlayer();
-        if(state == State.Moving)
-        {
-            Controll();
-        }
-        
-        
-    }
-
-    void Controll()
-    {
-
-        if (count % 2 == 0)
-        {
-            initiatePoint -= Time.deltaTime;
-        }
-        else if (count % 2 == 1)
-        {
-            initiatePoint += Time.deltaTime;
-        }
-        x = rangeX * Mathf.Cos(speedX * initiatePoint);
-        y = rangeY * Mathf.Sin(speedY * initiatePoint) + offSet;
-        transform.localPosition = new Vector3(x, y, 0);
-
     }
 
     public GameObject FindTheClosestPlayer()
@@ -128,36 +84,20 @@ public class BossFlyingMovement : Boss {
         }
         if (TargetPlayer.name == "Player1")
         {
-           
+
             this.transform.position = missionManager.GetBossPosition_P2();
-            offSet = rangeY * Mathf.Sin(speedY * initiatePoint) + this.transform.position.y;
             
+
         }
         else if (TargetPlayer.name == "Player2")
         {
-          
+
             this.transform.position = missionManager.GetBossPosition_P1();
-            offSet = rangeY * Mathf.Sin(speedY * initiatePoint) + this.transform.position.y;
             
+
         }
-        curPosition = this.transform.position;
-        CheckBossPosition();
         TargetPlayer = FindTheClosestPlayer();
         CurrentState = State.Moving;
-        initiatePoint = 0;
-
-    }
-
-    void CheckBossPosition()
-    {
-        if(curPosition.y >= 25)
-        {
-            inPlayer1 = true;
-        }
-        else
-        {
-            inPlayer1 = false;
-        }
     }
 
     void Die()
@@ -172,5 +112,6 @@ public class BossFlyingMovement : Boss {
         BossHealth.SwapingEvent -= SwapBoss;
         BossHealth.DeathEvent -= Die;
     }
+
 
 }
