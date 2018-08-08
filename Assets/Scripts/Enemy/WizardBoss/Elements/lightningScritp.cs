f7d3ab0495522d2b741f74f1bf96ac2e6d78632a
Thanawat <Spirit_windwalk@hotmail.com>
2018-08-09 05:31:45 +0700

    add lightning element

---
+++ Assets/Scripts/Enemy/WizardBoss/Elements/lightningScritp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightningScritp : MonoBehaviour {
+
+
+    void Start()
+    {
+        
+    }
+
+}
--- Assets/Scripts/Enemy/WizardBoss/WizardAttack.cs
+++ Assets/Scripts/Enemy/WizardBoss/WizardAttack.cs
@@ -5,29 +5,44 @@ using UnityEngine;
 public class WizardAttack : MonoBehaviour {
 
     [SerializeField]
+    private float stateTimer;
+    [SerializeField]
+    private float setStateTimer;
+    [SerializeField]
     private int numOfIceBullet;
     [SerializeField]
     private float rangeIceBullet;
     [SerializeField]
     private float speedIceBullet;
     [SerializeField]
-    private int magazine;
+    private float iceCooldown;
+    private float iceFire;
     [SerializeField]
-    private float reloadShot;
+    private float lightningCooldown;
     [SerializeField]
-    private float startShot;
+    private float lightningshot;
+    [SerializeField]
+    private float beforeLightning;
+    [SerializeField]
+    private float lightningDuration;
+
     
+    
+
+    public Vector3 lightningValue;
+    public GameObject blazeSprite;
+    public GameObject iceSprite;
+    public GameObject lightningSprite;
+    public GameObject lightPoint;
 
     ProjectilePool pool;
     private WizardMovement wizardMovement;
 
-    public GameObject bullet;
-
     public enum AttackElementType
     {
         ice,
-        fire,
-        thunder
+        blaze,
+        lightning
     }
     public AttackElementType attackElementType;
 
@@ -35,24 +50,22 @@ public class WizardAttack : MonoBehaviour {
     {
         pool = ProjectilePool.Instance;
         wizardMovement = GetComponent<WizardMovement>();
-        reloadShot = startShot;
+        iceFire = iceCooldown;
+        lightningshot = lightningCooldown;
     }
 
     public void AttackState()
     {
-        
         switch (attackElementType)
         {
             case AttackElementType.ice:
-                if(reloadShot <= 0)
-                {
-                    IceAttack(numOfIceBullet, rangeIceBullet, speedIceBullet);
-                    reloadShot = startShot;
-                }
-                else
-                {
-                    reloadShot -= Time.deltaTime;
-                }
+               
+                IceAttack(numOfIceBullet, rangeIceBullet, speedIceBullet);
+                break;
+
+            case AttackElementType.lightning:
+
+                StartCoroutine("lightingAttack");
                 
                 break;
         }
@@ -60,10 +73,18 @@ public class WizardAttack : MonoBehaviour {
 
     private void IceAttack(int numOfP, float range, float speedBullet)
     {
-        float angleTotal = 360f / numOfP;
-        float angle = 0f;
-        Vector3 projectileVec;
-        Vector3 projectileDir;
+        if (stateTimer <= 0)
+        {
+            wizardMovement.state = WizardMovement.State.Move;
+            stateTimer = setStateTimer;
+            iceFire = iceCooldown;
+        }
+        else if (iceFire <= 0.2)
+        {
+            float angleTotal = 360f / numOfP;
+            float angle = 0f;
+            Vector3 projectileVec;
+            Vector3 projectileDir;
             for (int j = 0; j < numOfP; j++)
             {
                 float projectileX = wizardMovement.FindTheClosestPlayer().transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180) * range;
@@ -72,13 +93,52 @@ public class WizardAttack : MonoBehaviour {
                 projectileVec = new Vector3(projectileX, projectileY, 0);
                 projectileDir = Vector3.Normalize(projectileVec - this.transform.position) * speedBullet;
 
-                bullet = pool.GetElementInPool("ice", transform.position, Quaternion.identity);
-                bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileDir.x, projectileDir.y);
+                iceSprite = pool.GetElementInPool("ice", transform.position, Quaternion.identity);
+                iceSprite.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileDir.x, projectileDir.y);
 
                 angle += angleTotal;
             }
-        wizardMovement.state = WizardMovement.State.Move;
+
+            iceFire = iceCooldown;
+        }
+        
+        else
+        {
+            iceFire -= Time.deltaTime;
+            stateTimer -= Time.deltaTime;
+            
+        }
+       
+    }
+
+    IEnumerator lightingAttack()
+    {
+        if (stateTimer <= 0)
+        {
+            wizardMovement.state = WizardMovement.State.Move;
+            stateTimer = setStateTimer;
+            lightningshot = lightningCooldown;
+        }
+        else if (lightningshot <= 0.2)
+        {
+            Vector3 lightningPosition = new Vector3(Random.Range(-lightningValue.x, lightningValue.x), 0, 0);
+            lightPoint = pool.GetElementInPool("lightPoint", lightningPosition + transform.TransformPoint(0, 4.5f, 0), gameObject.transform.rotation);
+            lightningshot = lightningCooldown;
+            yield return new WaitForSeconds(beforeLightning);
+            lightningSprite = pool.GetElementInPool("lightning", lightningPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+            yield return new WaitForSeconds(lightningDuration);
+            lightPoint.SetActive(false);
+            lightningSprite.SetActive(false);
+            
+        }
+        else
+        {
+            lightningshot -= Time.deltaTime;
+            stateTimer -= Time.deltaTime;
+        }
+  
     }
 
     
+
 }
