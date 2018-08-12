9fbaa29e8f349e44eaa9c6431d36789f9dc94efe
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2018-08-12 21:02:03 +0300

    add GameOver Manager

---
+++ Assets/Scripts/ScreenManager/GameOverManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverManager : MonoBehaviour {
+
+    private GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = GameManager.instance;
+    }
+
+    public void RestartGame()
+    {
+        gameManager.StartGame();
+    }
+
+    public void BackToMenu()
+    {
+        gameManager.LoadMenuScene();
+    }
+}
